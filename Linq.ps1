f97c4b03182c16e430325c6fcec89310a021caab
Stefan Hochrieser <stefan-h@gmx.at>
2023-05-02 17:56:26 +0200

    Init

---
+++ Linq.ps1
@@ -0,0 +1,46 @@
+﻿#https://www.red-gate.com/simple-talk/development/dotnet-development/high-performance-powershell-linq/#post-71022-_Toc482783697
+
+$Left = @()
+$Right = @()
+$ProductionStatusArray = @('In Production','Retired')
+$PowerStatusArray = @('Online','Offline')
+1..15 | Foreach-Object {
+    $Prop = @{
+        Name = "Server$_"
+        PowerStatus = $PowerStatusArray[(Get-Random -Minimum 0 -Maximum 2)]
+    }
+    $Left += New-Object -Type PSObject -Property $Prop
+}
+1..10 | Foreach-Object {
+    $Prop = @{
+        Name = "Server$_"
+        ProductionStatus = $ProductionStatusArray[(Get-Random -Minimum 0 -Maximum 2)]
+    }
+    $Right += New-Object -Type PSObject -Property $Prop
+}
+ 
+#In this instance both joins will be using the same property name so only one function is needed
+[System.Func[System.Object, string]]$JoinFunction = {
+    param ($x) 
+    $x.Name 
+}
+ 
+#This is the delegate needed in GroupJoin() method invocations
+[System.Func[System.Object, [Collections.Generic.IEnumerable[System.Object]], System.Object]]$query = {
+    param(
+        $LeftJoin,
+        $RightJoinEnum
+    )
+    $RightJoin = [System.Linq.Enumerable]::SingleOrDefault($RightJoinEnum)
+ 
+    New-Object -TypeName PSObject -Property @{
+        Name = $LeftJoin.Name; 
+        PowerStatus = $LeftJoin.PowerStatus; 
+        ProductionStatus = $RightJoin.ProductionStatus
+    }
+}
+ 
+#And lastly we call GroupJoin() and enumerate with ToArray()
+[System.Linq.Enumerable]::ToArray(
+    [System.Linq.Enumerable]::GroupJoin($Left, $Right, $JoinFunction, $JoinFunction, $query)
+)
---
+++ WinRM.ps1
@@ -0,0 +1,16 @@
+﻿#region
+#Enables WinRM and Config Firewall Rules
+Enable-PSRemoting
+#endregion
+
+#region
+#Mixed Domain require to allow from differnt Domain
+New-Itemproperty -name LocalAccountTokenFilterPolicy -path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -propertyType DWord -value 1
+Get-ItemProperty -Name LocalAccountTokenFilterPolicy -path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+#endregion
+
+#region
+#Trusted Hosts
+Get-Item wsman:localhost\client\trustedhosts
+Set-Item wsman:localhost\client\trustedhosts -value RM-Client1,RM-Client2
+#endregion
